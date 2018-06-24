d86fb64f12c3d86324335171bff14f5c97ee2a5c
Carl Tompkins <carl@ctomp.io>
2018-06-24 09:48:25 -0400

    Add sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Sidebar></Sidebar>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,6 +9,17 @@
   </div>
 </template>
 
+<script>
+// @ is an alias to /src
+import Sidebar from "@/components/Sidebar.vue";
+
+export default {
+  components: {
+    Sidebar
+  }
+};
+</script>
+
 <style lang="scss">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
@@ -15,6 +27,9 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  position: relative;
 }
 #nav {
   padding: 30px;
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+    <aside id="sidebar">
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Analytics</li>
+                <li>Reach</li>
+                <li>Campaign</li>
+                <li>Publishing</li>
+                <li>Manage Account</li>
+            </ul>
+        </nav>
+    </aside>
+</template>
+
+<script>
+export default {
+  name: "Sidebar"
+};
+</script>
+
+<style scoped lang="scss">
+#sidebar {
+  background-color: #333333;
+  color: #ffffff;
+  position: absolute;
+  height: 100%;
+}
+</style>
